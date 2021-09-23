89f9bca0c500eed48338be43eeb5e8088f657d41
Syed Qasim <87519159+syedqaxim110@users.noreply.github.com>
2021-09-23 15:01:42 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string _inputstr, _reversestr = string.Empty;
+            Console.Write("Enter a string : ");
+            _inputstr = Console.ReadLine();
+            if (_inputstr != null)
+            {
+                for (int i = _inputstr.Length - 1; i >= 0; i--)
+                {
+                    _reversestr += _inputstr[i].ToString();
+                }
+                if (_reversestr == _inputstr)
+                {
+                    Console.WriteLine("String is Palindrome Input = {0} and Output= {1}", _inputstr, _reversestr);
+                }
+                else
+                {
+                    Console.WriteLine("String is not Palindrome Input = {0} and Output= {1}", _inputstr, _reversestr);
+                }
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
+
